e737fde6d7b4c15d842e97751955e4bef85d0563
Lance Tian <xtian513@gmail.com>
2019-05-10 02:18:00 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, './src')  
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
